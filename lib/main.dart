602b9df379c8954ad7f9ea92e2d11a8c740e3e6d
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-01 16:46:52 -0300

    Modifying the syntax to an shorter and cleaner code

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
-}
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   build(context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Flutter Playground')),
+        appBar: AppBar(
+          title: Text('Flutter Playground'),
+        ),
       ),
     );
   }

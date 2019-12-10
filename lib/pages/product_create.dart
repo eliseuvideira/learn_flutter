f93c1a511ef2e1ef5e5501ed90134dd77a2c4373
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-10 19:32:21 -0300

    Finishing the Tab Navigation

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Center(
+      child: Text('Create a Product'),
+    );
   }
 }
--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class ProductListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Center(
+      child: Text('All Products'),
+    );
   }
 }

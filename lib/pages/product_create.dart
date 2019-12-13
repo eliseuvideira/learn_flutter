1065635b9f3d16172808a44fa2efdef900db7ad8
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-12 21:19:43 -0300

    Saving user input & Configuring text fields

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  _ProductCreatePageState createState() => _ProductCreatePageState();
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String title;
+  String description;
+  double price;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (context) {
-              return Center(
-                child: Text('This is a modal'),
-              );
-            },
-          );
-        },
-      ),
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              title = value;
+            });
+          },
+        ),
+        TextField(
+          maxLines: 4,
+          onChanged: (String value) {
+            setState(() {
+              description = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          onChanged: (String value) {
+            setState(() {
+              price = double.parse(value);
+            });
+          },
+        ),
+      ],
     );
   }
 }

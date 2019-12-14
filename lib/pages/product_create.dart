b477c17fd490c6579689e5330759641b1ad16e37
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-13 21:01:49 -0300

    Improving the style of our form

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -46,8 +46,11 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
               });
             },
           ),
+          SizedBox(height: 10.0),
           RaisedButton(
             child: Text('Save'),
+            color: Theme.of(context).accentColor,
+            textColor: Colors.white,
             onPressed: () {
               final Map<String, dynamic> product = {
                 'title': title,
@@ -56,6 +59,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
                 'image': 'assets/food.jpg'
               };
               widget.addProduct(product);
+              Navigator.pushReplacementNamed(context, '/');
             },
           )
         ],
